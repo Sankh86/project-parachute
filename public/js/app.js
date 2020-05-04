1eb7013c87a97ef7d55e4d9896f45b09450ac099
David <sancrock@gmail.com>
2020-05-04 16:56:25 -0400

    Therapist query created

--- public/js/app.js
+++ public/js/app.js
@@ -25,6 +25,34 @@ function cleanString(string) {
     return str;
 }
 
+function htmlString(string) {
+    let str = "";
+    if(string.length > 0) {
+        str = string.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    }
+    return str;
+}
+
+function convertTime(time) {
+    let output = "";
+    if (time.length > 0) {
+        hour = parseInt(time.substr(0, time.indexOf(':')));
+        minutes = time.substr(time.indexOf(':'));
+        suffix = "AM"
+        if (hour > 11) {
+            suffix = "PM"
+        }
+        if (hour > 12) {
+            hour = hour - 12;
+        }
+        if (hour === "0" || hour === "00") {
+            hour = 12;
+        }
+        output = `${hour}${minutes} ${suffix}`
+    }
+    return output;
+}
+
 
 //  ***** Upload Therapist Data *****
 function uploadTherapistData() {
@@ -88,12 +116,8 @@ function uploadTherapistData() {
         "insurance": "${onboardInsurance}",
         "picture": "${onboardPicture}",
         "description": "${onboardDescription}",
-        "licenseStateA": "${onboardStateA}",
-        "licenseNoA": "${onboardLicenseNoA}",
-        "licenseStateB": "${onboardStateB}",
-        "licenseNoB": "${onboardLicenseNoB}",
-        "licenseStateC": "${onboardStateC}",
-        "licenseNoC": "${onboardLicenseNoC}",
+        "licenseStateList": ["${onboardStateA}", "${onboardStateB}", "${onboardStateC}"],
+        "licenseStateIDList": ["${onboardLicenseNoA}", "${onboardLicenseNoB}", "${onboardLicenseNoC}"],
         "timeZone": "${onboardTimezone}",
         "Availability": {
             "mon": {
@@ -126,7 +150,8 @@ function uploadTherapistData() {
             }
         },
         "exposeInfo": ${onboardConsent},
-        "isVerified": false,
+        "isVerified": true,
+        "showProfile": true,
         "createdDate": "${onboardCurrentTime}"
     }`
 
@@ -154,9 +179,11 @@ function uploadTherapistData() {
 $('#onboardForm').hide();
 $('#studentEmail').hide();
 $('#provisionalInfo').hide();
+$('#liabilityRadioBox').hide();
+$('#liabilityInfo').hide();
 
 $('input:radio[name="onboardLicenseType"]').change(function() {
-    $('#onboardForm').show();
+    $('#liabilityRadioBox').show();
     if ($(this).val() == 'Licensed') {
             $('#supervisorNameBox').hide();
             $('#stateBoxB').show();
@@ -171,7 +198,16 @@ $('input:radio[name="onboardLicenseType"]').change(function() {
 
     });
 
-
+$('input:radio[name="liabilityCheck"]').change(function() {
+    $('#liabilityRadioBox').hide();
+    if ($(this).val() == 'Yes') {
+        $('#onboardForm').show();
+        $('#liabilityInfo').hide();
+    } else {
+        $('#liabilityInfo').show();
+        $('#onboardForm').hide();
+    }
+});
 
 
 //  ***** Preview Profile Image *****
@@ -481,6 +517,224 @@ $('#queryState').change(function() {
     }
 });
 
+//  ********** Therapist Query **********
+$('#therapistResults').hide();
+let queryData =  []
+$( "#queryTherapists" ).click(function() {
+    $('#questionnaire').hide();
+    $('#therapistResults').show();
+    queryData =  [];
+    const stateLookup = $('#queryState').val();
+    
+    db.collection('Therapists')
+        .where("licenseStateList", "array-contains", stateLookup)
+        .where("showProfile", "==", true)
+        .where("isVerified", "==", true)
+        .get().then((snap) => {
+        snap.forEach((doc) => {
+            let n = queryData.length
+            queryData[n] = {};
+            queryData[n]['id'] = doc.id;
+            queryData[n]['availability'] = doc.data().Availability;
+            queryData[n]['cityState'] = doc.data().cityState;
+            queryData[n]['description'] = doc.data().description;
+            queryData[n]['licenseType'] = doc.data().licenseType;
+            queryData[n]['name'] = doc.data().name;
+            queryData[n]['picture'] = doc.data().picture;
+            queryData[n]['timeZone'] = doc.data().timeZone;
+            queryData[n]['insurance'] = doc.data().insurance;
+            queryData[n]['sortValue'] = Math.random();
+        });
+    }).then(() => {
+        queryData.sort((a, b) => a.sortValue - b.sortValue);
+
+        $.each(queryData, function(index, value) {
+            const id = queryData[index]['id']
+
+            const profileHTML = `    <section class="profileWrap">
+                                        <div class="profileImageWrap">
+                                            <img id="${id}-picture" class="profileImage" src="#" alt="Your Profile Picture" />
+                                        </div>
+                                        <div class="profileInfoWrap">
+                                            <div class="profileTitle">
+                                                <h2 id="${id}-name"></h2>
+                                            </div>
+                                            <div class="profileDetails">
+                                                <p id="${id}-cityState" style="font-weight: bold;"></p>
+                                                <p id="${id}-insurance" style="font-weight: bold; font-size: 18px;"></p>
+                                                <p id="${id}-description" class="profileDescription"></p>
+                                            </div>
+                                        </div>
+                                        <div class="availabilityWrap">
+                                            <p id="${id}-timeZone" class="availabilityTitle"></p>
+                                            <p id="${id}-availability" class="availabilityBlock"></p>
+                                            <button class="requestApptBtn" id="${id}-requestAppt">Request Appointment</button>
+                                        </div>
+                                    </section>  `
+            $('#therapistResults').append(profileHTML);
+            let licenseType = ""
+            if (queryData[index]['licenseType'] !== "") {
+                licenseType = `, ${queryData[index]['licenseType']}`
+            }
+            $(`#${id}-name`).html(`${queryData[index]['name']}${licenseType}`);
+            
+            if (queryData[index]['cityState'] !== "") {
+                $(`#${id}-cityState`).html(`${queryData[index]['cityState']}`);
+            }
+            if (queryData[index]['insurance'] !== "") {
+                $(`#${id}-insurance`).html(`Insurance: ${queryData[index]['insurance']}`);
+            }
+            if (queryData[index]['description'] !== "") {
+                $(`#${id}-description`).html(JSON.parse(`"${queryData[index]['description']}"`));
+            }
+            $(`#${id}-timeZone`).html(`Availability [${queryData[index]['timeZone']}] :`);
+        
+            let availability = ""
+            if (queryData[index]['availability']['mon']['startTime'] !== "" && queryData[index]['availability']['mon']['endTime'] !== "") {
+                availability += `Mon: ${convertTime(queryData[index]['availability']['mon']['startTime'])} to ${convertTime(queryData[index]['availability']['mon']['endTime'])}`
+            }
+            if (queryData[index]['availability']['tue']['startTime'] !== "" && queryData[index]['availability']['tue']['endTime'] !== "") {
+                if (availability.length > 0) {availability += "<br>"}
+                availability += `Tue: ${convertTime(queryData[index]['availability']['tue']['startTime'])} to ${convertTime(queryData[index]['availability']['tue']['endTime'])}`
+            }
+            if (queryData[index]['availability']['wed']['startTime'] !== "" && queryData[index]['availability']['wed']['endTime'] !== "") {
+                if (availability.length > 0) {availability += "<br>"}
+                availability += `Wed: ${convertTime(queryData[index]['availability']['wed']['startTime'])} to ${convertTime(queryData[index]['availability']['wed']['endTime'])}`
+            }
+            if (queryData[index]['availability']['thu']['startTime'] !== "" && queryData[index]['availability']['thu']['endTime'] !== "") {
+                if (availability.length > 0) {availability += "<br>"}
+                availability += `Thu: ${convertTime(queryData[index]['availability']['thu']['startTime'])} to ${convertTime(queryData[index]['availability']['thu']['endTime'])}`
+            }
+            if (queryData[index]['availability']['fri']['startTime'] !== "" && queryData[index]['availability']['fri']['endTime'] !== "") {
+                if (availability.length > 0) {availability += "<br>"}
+                availability += `Fri: ${convertTime(queryData[index]['availability']['fri']['startTime'])} to ${convertTime(queryData[index]['availability']['fri']['endTime'])}`
+            }
+            if (queryData[index]['availability']['sat']['startTime'] !== "" && queryData[index]['availability']['sat']['endTime'] !== "") {
+                if (availability.length > 0) {availability += "<br>"}
+                availability += `Sat: ${convertTime(queryData[index]['availability']['sat']['startTime'])} to ${convertTime(queryData[index]['availability']['sat']['endTime'])}`
+            }
+            if (queryData[index]['availability']['sun']['startTime'] !== "" && queryData[index]['availability']['sun']['endTime'] !== "") {
+                if (availability.length > 0) {availability += "<br>"}
+                availability += `Sun: ${convertTime(queryData[index]['availability']['sun']['startTime'])} to ${convertTime(queryData[index]['availability']['sun']['endTime'])}`
+            }
+            $(`#${id}-availability`).html(availability);
+
+
+            if (queryData[index]['picture'] !== "") {
+               storage.ref(queryData[index]['picture']).getDownloadURL().then((url) => {
+                    $(`#${id}-picture`).attr('src', url)
+                }).catch(err => {
+                    $(`#${id}-picture`).attr('src', 'img/parachute-logo.png')
+                });
+            } else {
+                $(`#${id}-picture`).attr('src', 'img/parachute-logo.png');
+            }
+        });
+    });
+});
+
+
+
+//  ********** TEMP PROFILE **********
+// let = testProfile = {} ;
+// let profileURL = "";
+// db.collection("Therapists").doc("4FSK7p65RrSj7Vlsb974s941bgC3").get().then((snapshot) => {
+//     testProfile.id = snapshot.id;
+//     testProfile.availability = snapshot.data().Availability;
+//     testProfile.cityState = snapshot.data().cityState;
+//     testProfile.description = snapshot.data().description;
+//     testProfile.licenseType = snapshot.data().licenseType;
+//     testProfile.name = snapshot.data().name;
+//     testProfile.picture = snapshot.data().picture;
+//     testProfile.timeZone = snapshot.data().timeZone;
+//     testProfile.insurance = snapshot.data().insurance;
+
+//     let licenseType = ""
+//     if (testProfile.licenseType !== "") {
+//         licenseType = `, ${testProfile.licenseType}`
+//     }
+//     $(`#${testProfile.id}-name`).html(`${testProfile.name}${licenseType}`);
+//     if (testProfile.cityState !== "") {
+//         $(`#${testProfile.id}-cityState`).html(`${testProfile.cityState}`);
+//     }
+//     if (testProfile.insurance !== "") {
+//         $(`#${testProfile.id}-insurance`).html(`Insurance: ${testProfile.insurance}`);
+//     }
+//     if (testProfile.description !== "") {
+//         $(`#${testProfile.id}-description`).html(JSON.parse(`"${testProfile.description}"`));
+//     }
+//     $(`#${testProfile.id}-timeZone`).html(`Availability [${testProfile.timeZone}] :`);
+
+//     let availability = ""
+//     if (testProfile.availability.mon.startTime !== "" && testProfile.availability.mon.endTime !== "") {
+//         availability += `Mon: ${convertTime(testProfile.availability.mon.startTime)} to ${convertTime(testProfile.availability.mon.endTime)}`
+//     }
+//     if (testProfile.availability.tue.startTime !== "" && testProfile.availability.tue.endTime !== "") {
+//         if (availability.length > 0) {availability += "<br>"}
+//         availability += `Tue: ${convertTime(testProfile.availability.tue.startTime)} to ${convertTime(testProfile.availability.tue.endTime)}`
+//     }
+//     if (testProfile.availability.wed.startTime !== "" && testProfile.availability.wed.endTime !== "") {
+//         if (availability.length > 0) {availability += "<br>"}
+//         availability += `Wed: ${convertTime(testProfile.availability.wed.startTime)} to ${convertTime(testProfile.availability.wed.endTime)}`
+//     }
+//     if (testProfile.availability.thu.startTime !== "" && testProfile.availability.thu.endTime !== "") {
+//         if (availability.length > 0) {availability += "<br>"}
+//         availability += `Thu: ${convertTime(testProfile.availability.thu.startTime)} to ${convertTime(testProfile.availability.thu.endTime)}`
+//     }
+//     if (testProfile.availability.fri.startTime !== "" && testProfile.availability.fri.endTime !== "") {
+//         if (availability.length > 0) {availability += "<br>"}
+//         availability += `Fri: ${convertTime(testProfile.availability.fri.startTime)} to ${convertTime(testProfile.availability.fri.endTime)}`
+//     }
+//     if (testProfile.availability.sat.startTime !== "" && testProfile.availability.sat.endTime !== "") {
+//         if (availability.length > 0) {availability += "<br>"}
+//         availability += `Sat: ${convertTime(testProfile.availability.sat.startTime)} to ${convertTime(testProfile.availability.sat.endTime)}`
+//     }
+//     if (testProfile.availability.sun.startTime !== "" && testProfile.availability.sun.endTime !== "") {
+//         if (availability.length > 0) {availability += "<br>"}
+//         availability += `Sun: ${convertTime(testProfile.availability.sun.startTime)} to ${convertTime(testProfile.availability.sun.endTime)}`
+//     }
+//     $(`#${testProfile.id}-availability`).html(availability);
+
+// }).then(() => {
+//     storage.ref(testProfile.picture).getDownloadURL().then((url) => {
+//         testProfile.picture = url;
+//         $(`#${testProfile.id}-picture`).attr('src', testProfile.picture);
+//     });
+// });
+
+
+// var testKentucky = db.collectionGroup('Therapists').where('LicenseStateA', '==', 'Kentucky');
+// testKentucky.get().then(function (querySnapshot) {
+//     querySnapshot.forEach(function (doc) {
+//         console.log(doc.id, ' => ', doc.data());
+//     });
+// });
+
+// let queryData =  []
+// db.collection('Therapists')
+//     .where("licenseStateList", "array-contains", "Kentucky")
+//     .where("showProfile", "==", true)
+//     .where("isVerified", "==", true)
+//     .get().then((snap) => {
+//     snap.forEach((doc) => {
+//         let n = queryData.length
+//         queryData[n] = {};
+//         queryData[n]['id'] = doc.id;
+//         queryData[n]['availability'] = doc.data().Availability;
+//         queryData[n]['cityState'] = doc.data().cityState;
+//         queryData[n]['description'] = doc.data().description;
+//         queryData[n]['licenseType'] = doc.data().licenseType;
+//         queryData[n]['name'] = doc.data().name;
+//         queryData[n]['picture'] = doc.data().picture;
+//         queryData[n]['timeZone'] = doc.data().timeZone;
+//         queryData[n]['insurance'] = doc.data().insurance;
+//         queryData[n]['sortValue'] = Math.random();
+//     });
+// }).then(() => {
+//     queryData.sort((a, b) => a.sortValue - b.sortValue);
+// });
+
+
 
 
 
@@ -492,3 +746,47 @@ $('#queryState').change(function() {
 // storageRef.getDownloadURL().then(function(url) {
                 
 //     onboardPicture = url
+
+
+
+// db.collection('Therapists').get().then((snap) => {
+//     therapistArray = snap.data();
+// });
+
+
+
+var therapistArray = {}
+function fetchDB() {
+    therapistArray = {}
+    db.collection("Therapists").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            therapistArray[doc.id] = doc.data();
+
+
+        });
+    });
+}
+
+// function updateDB() {
+//     let recordCount = Object.keys(therapistArray).length
+//     $.each(therapistArray, function(index, value) {
+//         therapistArray[index]['licenseStateList'] = [value.licenseStateA, value.licenseStateB, value.licenseStateC];
+//         therapistArray[index]['licenseStateIDList'] = [value.licenseNoA, value.licenseNoB, value.licenseNoC];
+//         if(!('showProfile' in therapistArray[index])) {
+//             therapistArray[index]['showProfile'] = true;
+//         }
+//     });
+//     console.log(`${recordCount} updated`)
+
+//     let currentRecord = 0;
+//     $.each(therapistArray, function(index, value) {
+//         db.collection('Therapists').doc(index).update(therapistArray[index]).then(() => {
+//             currentRecord += 1;
+//             console.log(`${index} loaded (${currentRecord}/${recordCount})`);    
+//         });
+//     });     
+
+
+
+// }
